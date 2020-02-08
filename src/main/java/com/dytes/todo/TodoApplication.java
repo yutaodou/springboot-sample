a948b1b08bde41e835e5e54873f47834d413dede
Yutao Dou <yutaodou@gmail.com>
2020-02-08 15:29:47 +0800

    configure static code analysizer checkstyle & PMD

--- src/main/java/com/dytes/todo/TodoApplication.java
+++ src/main/java/com/dytes/todo/TodoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 }
--- src/test/java/com/dytes/todo/TodoApplicationTests.java
+++ src/test/java/com/dytes/todo/TodoApplicationTests.java
@@ -1,12 +1,12 @@
 package com.dytes.todo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class TodoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertThat(2).isEqualTo(2);
+    }
 }
