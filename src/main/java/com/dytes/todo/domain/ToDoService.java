f1ae728dc7db9af7f5c31f606aa8296ca406e02b
Dou Yutao <yutaodou@gmail.com>
2020-05-15 13:40:25 +0800

    move repo to infrastructure layer

--- src/main/java/com/dytes/todo/domain/ToDoService.java
+++ src/main/java/com/dytes/todo/domain/ToDoService.java
@@ -1,5 +1,6 @@
 package com.dytes.todo.domain;
 
+import com.dytes.todo.infrastructure.todo.ToDoRepository;
 import com.dytes.todo.share.event.DomainEventPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/dytes/todo/infrastructure/todo/ToDoRepository.java
+++ src/main/java/com/dytes/todo/infrastructure/todo/ToDoRepository.java
@@ -1,5 +1,6 @@
-package com.dytes.todo.domain;
+package com.dytes.todo.infrastructure.todo;
 
+import com.dytes.todo.domain.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
